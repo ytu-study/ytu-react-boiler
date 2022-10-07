380ed01e2fd7e05a4cbe45bb59bcbaf3254aa22f
ukyi <devcat.js@gmail.com>
2022-10-07 23:45:47 +0900

    🔇 log 제거

--- packages/sanguk/src/pages/index.tsx
+++ packages/sanguk/src/pages/index.tsx
@@ -1,7 +1,3 @@
-import { useMatches } from 'react-router-dom';
-
 export default function Index(): JSX.Element {
-  const matches = useMatches();
-  console.log('### matches', matches);
   return <div>Index</div>;
 }
