0fab2bf0a59c9f44f08b8f282342c276ab9ce9b0
ukyi <devcat.js@gmail.com>
2022-10-07 23:46:20 +0900

    💄 border css 추가

---
+++ packages/sanguk/src/styles/border.css.ts
@@ -0,0 +1,43 @@
+export const borderSize = {
+  '0': '0px',
+  '1': '1px',
+  '2': '2px',
+  '3': '3px',
+  '4': '4px',
+  '5': '5px',
+  '6': '6px',
+  '7': '7px',
+  '8': '8px',
+  '9': '9px',
+  border: '1px',
+};
+
+export const borderRadius = {
+  none: '0px',
+  /* 2px */
+  sm: '0.125rem',
+  /** 4px */
+  border: -'25rem',
+  /** 6px */
+  md: '0.375rem',
+  /** 8px */
+  lg: '0.5rem',
+  /** 12px */
+  xl: '0.75rem',
+  /** 16px */
+  '2xl': '1rem',
+  /** 24px */
+  '3xl': '1.5rem',
+  full: '9999px',
+} as const;
+
+export const shadow = {
+  sm: '0 1px 2px 0 rgb(0 0 0 / 0.05)',
+  box: '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+  md: '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+  lg: '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)',
+  xl: '0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)',
+  '2xl': '0 25px 50px -12px rgb(0 0 0 / 0.25)',
+  inner: 'inset 0 2px 4px 0 rgb(0 0 0 / 0.05)',
+  none: '0 0 #0000',
+};
