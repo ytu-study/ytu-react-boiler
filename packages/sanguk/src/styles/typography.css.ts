6cfa86fe9fd16d1529e45bd254cba92db4d2ead5
ukyi <devcat.js@gmail.com>
2022-10-07 23:47:10 +0900

    💄 typography css 추가

---
+++ packages/sanguk/src/styles/typography.css.ts
@@ -0,0 +1,80 @@
+export const fontFamily = {
+  'sans-serif': 'system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif;',
+  serif: 'ui-serif, serif;',
+  mono: 'Dank Mono, Operator Mono, Inconsolata, Fira Mono, ui-monospace, SF Mono, Monaco, Droid Sans Mono, Source Code Pro, monospace;',
+} as const;
+
+export const fontWeight = {
+  thin: 100,
+  extralight: 200,
+  light: 300,
+  normal: 400,
+  medium: 500,
+  semibold: 600,
+  bold: 700,
+  extrabold: 800,
+  black: 900,
+} as const;
+
+export const fontStyle = {
+  italic: 'italic',
+  'not-italic': 'normal',
+} as const;
+
+export const lineHeight = {
+  /** 4px */
+  '1': '.25rem',
+  /** 8px */
+  '2': '.5rem',
+  /** 12px */
+  '3': '.75rem',
+  /** 16px */
+  '4': '1rem',
+  /** 20px */
+  '5': '1.25rem',
+  /** 24px */
+  '6': '1.5rem',
+  /** 28px */
+  '7': '1.75rem',
+  /** 32px */
+  '8': '2rem',
+  /** 36px */
+  '9': '2.25rem',
+  /** 40px */
+  '10': '2.5rem',
+  none: '1',
+  tight: '1.25',
+  snug: '1.375',
+  normal: '1.5',
+  relaxed: '1.625',
+  loose: '2',
+} as const;
+
+export const fontSize = {
+  /** 12px */
+  xs: '0.75rem',
+  /** 14px */
+  sm: '0.875rem',
+  /** 16px */
+  base: '1rem',
+  /** 18px */
+  lg: '1.125rem',
+  /** 20px */
+  xl: '1.25rem',
+  /** 24px */
+  '2xl': '1.5rem',
+  /** 30px */
+  '3xl': '1.875rem',
+  /** 36px */
+  '4xl': '2.25rem',
+  /** 48px */
+  '5xl': '3rem',
+  /** 60px */
+  '6xl': '3.75rem',
+  /** 72px */
+  '7xl': '4.5rem',
+  /** 96px */
+  '8xl': '6rem',
+  /** 128px */
+  '9xl': '8rem',
+} as const;
