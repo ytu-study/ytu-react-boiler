aba513ba0e550f7bf34d4392cb11531d57bf242e
ukyi <devcat.js@gmail.com>
2022-10-12 01:30:16 +0900

    ✨ Box 컴포넌트 추가

--- packages/sanguk/src/components/layout/header.tsx
+++ packages/sanguk/src/components/layout/header.tsx
@@ -1,5 +1,24 @@
 import type { PropsWithChildren } from 'react';
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { themeState } from '@/store';
+import Box from '@/components/Box';
 
 export default function Header({ children }: PropsWithChildren): JSX.Element {
-  return <header></header>;
+  const [theme, setTheme] = useRecoilState(themeState);
+
+  useEffect(() => {
+    const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    setTheme(isDark ? 'dark' : 'light');
+  }, []);
+
+  const toggleTheme = () => setTheme(theme === 'dark' ? 'light' : 'dark');
+
+  return (
+    <Box tag="header" display="flex">
+      <Box tag="button" display="inline-flex" marginLeft="auto" paddingX="2" paddingY="2" onClick={toggleTheme}>
+        theme: {theme}
+      </Box>
+    </Box>
+  );
 }
--- packages/sanguk/src/components/layout/index.tsx
+++ packages/sanguk/src/components/layout/index.tsx
@@ -1,23 +1,14 @@
-import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
-import { sprinkles } from '@/styles/sprinkles.css';
+import Box from '@/components/Box';
+import Header from '@/components/layout/header';
 
 export default function Layout(): JSX.Element {
-  useEffect(() => {
-    const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    document.documentElement.classList.add(isDark ? 'dark' : 'light');
-  }, []);
-
   return (
-    <div id="layout">
-      <div
-        className={sprinkles({
-          display: 'flex',
-          color: 'blue500',
-          paddingY: '4',
-          width: 'full',
-        })}></div>
-      <Outlet />
-    </div>
+    <>
+      <Header />
+      <Box id="layout" display="flex" width="full">
+        <Outlet />
+      </Box>
+    </>
   );
 }
