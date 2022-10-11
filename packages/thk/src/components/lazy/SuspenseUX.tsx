340c85310a3b49444cb99522c1debade64df5aef
TtaengG <taehyung.kim@myrealtrip.com>
2022-10-11 23:03:16 +0900

    [feat] useTransition 예시 추가중

--- packages/thk/src/components/lazy/SuspenseUX.tsx
+++ packages/thk/src/components/lazy/SuspenseUX.tsx
@@ -1,3 +1,35 @@
+import { Suspense, useState, useTransition } from 'react';
+import type { ProfileDataType } from '@/api/fetchProfileData';
+import { fetchProfileData } from '@/api/fetchProfileData';
+
+const initResource = fetchProfileData(0);
+
 export default function SuspenseUX(): JSX.Element {
-  return <div>SuspenseUX</div>;
+  const [resource, setResource] = useState(initResource);
+  const [isPending, startTransition] = useTransition();
+
+  const handleClick = () => {
+    const nextUserId = (resource.userId + 1) % 4;
+    setResource(fetchProfileData(nextUserId));
+  };
+
+  return (
+    <>
+      <button onClick={handleClick}>Next</button>
+      <ProfilePage resource={resource} />
+    </>
+  );
+}
+
+interface ResourceProps {
+  resource: ProfileDataType;
+}
+
+function ProfilePage({ resource }: ResourceProps): JSX.Element {
+  return <Suspense fallback={<h2>Loading Profile...</h2>}></Suspense>;
+}
+
+function ProfileName({ resource }: ResourceProps): JSX.Element {
+  const user = resource.user.read();
+  return <h1>{user?.name}</h1>;
 }
