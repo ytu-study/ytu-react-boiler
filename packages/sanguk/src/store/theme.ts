b80a82866b52fd773dc5e3683fc28f2206764254
ukyi <devcat.js@gmail.com>
2022-10-12 01:25:54 +0900

    ✨ themeEffect 추가

--- packages/sanguk/src/store/theme.ts
+++ packages/sanguk/src/store/theme.ts
@@ -1,6 +1,17 @@
+import type { AtomEffect } from 'recoil';
 import { atom } from 'recoil';
 
-export const themeState = atom({
+export type Theme = 'dark' | 'light';
+const themeEffect = ({ onSet }: Parameters<AtomEffect<Theme>>[0]) => {
+  onSet((newValue, oldValue) => {
+    const rootClass = document.documentElement.classList;
+    rootClass.remove(oldValue as Theme);
+    rootClass.add(newValue);
+  });
+};
+
+export const themeState = atom<Theme>({
   key: 'themeState',
   default: 'dark',
+  effects: [themeEffect],
 });
