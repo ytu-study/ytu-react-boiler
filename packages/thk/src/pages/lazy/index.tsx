2654457da01724333cdb0af2cc73cd1c287c59c5
Abangpa1ace <dyek72731@gmail.com>
2022-10-18 01:56:47 +0900

    Suspense + React Query 예시 추가완료

--- packages/thk/src/pages/lazy/index.tsx
+++ packages/thk/src/pages/lazy/index.tsx
@@ -9,19 +9,19 @@ import ExampleUXs from '@/components/lazy/ExampleUXs';
 export default function LazyPage(): JSX.Element {
   return (
     <div className="flex flex-col p-[40px] [&>div]:w-full [&>div]:h-[200px] [&>div]:border-dark-200 [&>div]:border-2">
-      <h2 className="mt-[30px] mb-[8px] text-[20px] font-bold">ExampleSuspenseBasic</h2>
+      <h2 className="mt-[30px] mb-[8px] text-[20px] font-bold">Example: SuspenseBasic</h2>
       <ExampleSuspenseBasic />
-      <h2 className="mt-[30px] mb-[8px] text-[20px] font-bold">ExampleSuspenseList</h2>
+      <h2 className="mt-[30px] mb-[8px] text-[20px] font-bold">Example: SuspenseList</h2>
       <ExampleSuspenseList />
-      <h2 className="mt-[30px] mb-[8px] text-[20px] font-bold">ExampleErrorBoundary</h2>
+      <h2 className="mt-[30px] mb-[8px] text-[20px] font-bold">Example: ErrorBoundary</h2>
       <ExampleErrorBoundary />
-      <h2 className="mt-[30px] mb-[8px] text-[20px] font-bold">ExampleReactQuery</h2>
+      <h2 className="mt-[30px] mb-[8px] text-[20px] font-bold">Example: ReactQuery</h2>
       <ExampleReactQuery />
-      <h2 className="mt-[30px] mb-[8px] text-[20px] font-bold">ExampleTransition</h2>
+      <h2 className="mt-[30px] mb-[8px] text-[20px] font-bold">Example: Transition</h2>
       <ExampleTransition />
-      <h2 className="mt-[30px] mb-[8px] text-[20px] font-bold">ExampleUXs</h2>
+      <h2 className="mt-[30px] mb-[8px] text-[20px] font-bold">Example: UXs</h2>
       <ExampleUXs />
-      <h2 className="mt-[30px] mb-[8px] text-[20px] font-bold">ExampleDeferredValue</h2>
+      <h2 className="mt-[30px] mb-[8px] text-[20px] font-bold">Example: DeferredValue</h2>
       <ExampleDeferredValue />
     </div>
   );
