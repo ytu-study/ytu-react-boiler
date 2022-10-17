6929881b5cec85924b7e0c2654a748fb1b237cc3
Abangpa1ace <dyek72731@gmail.com>
2022-10-18 02:04:48 +0900

    Input Validator 세팅중

---
+++ packages/thk/src/pages/input-validator/index.tsx
@@ -0,0 +1,8 @@
+export default function InputValidatorPage(): JSX.Element {
+  return (
+    <div>
+      <h2>input-validator</h2>
+      <input />
+    </div>
+  );
+}
---
+++ shared/hooks/useInput.tsx
@@ -0,0 +1,5 @@
+const useInput = () => {
+  return null;
+};
+
+export default useInput;
