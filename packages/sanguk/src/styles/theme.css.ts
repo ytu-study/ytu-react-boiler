881331b8dffa3bc4e80b551cd083d3a2762a1ed8
ukyi <devcat.js@gmail.com>
2022-10-17 23:36:40 +0900

    💄 themeColors, dark, light globalTheme 추가

--- packages/sanguk/src/styles/theme.css.ts
+++ packages/sanguk/src/styles/theme.css.ts
@@ -1,5 +1,6 @@
-import { createGlobalTheme } from '@vanilla-extract/css';
+import { createGlobalTheme, createGlobalThemeContract } from '@vanilla-extract/css';
 import { darkColor, lightColor } from '@/styles/mainColor.css';
 
-export const darkThemeVars = createGlobalTheme(':root.dark', darkColor);
-export const lightThemeVars = createGlobalTheme(':root.light', lightColor);
+export const themeColors = createGlobalThemeContract(darkColor, (_value, path) => `${path.join('')}`);
+createGlobalTheme(':root.dark', themeColors, darkColor);
+createGlobalTheme(':root.light', themeColors, lightColor);
