25a82f30561c7dde8c06698f5acbd18c53b720ae
TtaengG <taehyung.kim@myrealtrip.com>
2022-10-18 23:54:24 +0900

    feat: Input Validator 세팅중

--- shared/hooks/useInput.tsx
+++ shared/hooks/useInput.tsx
@@ -1,5 +1,28 @@
-const useInput = () => {
-  return null;
+import { useState, useCallback } from 'react';
+
+interface Options {
+  initValue?: string;
+  validators?: string[];
+}
+
+interface Returns {
+  // value: string;
+  setValue: (value: string) => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const useInput = ({ initValue }: Options): Returns => {
+  const [value, setValue] = useState<string>(initValue || '');
+  const [valid, setValid] = useState(0);
+
+  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = e;
+    setValue(value);
+  }, []);
+
+  return { setValue, onChange };
 };
 
 export default useInput;
