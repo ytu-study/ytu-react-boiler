11ead1bea502d7276cb9a0b4a89675ebd5ad5c40
ukyi <devcat.js@gmail.com>
2022-10-19 00:21:24 +0900

    🚚 navbar로 수정

--- packages/sanguk/src/components/layout/index.tsx
+++ packages/sanguk/src/components/layout/index.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import { Main } from '@/components/common';
-import Gnb from '@/components/layout/gnb';
+import Navbar from '@/components/layout/navbar';
 
 export default function Layout(): JSX.Element {
   return (
     <>
-      <Gnb />
+      <Navbar />
       <Main id="layout" display="flex" width="full">
         <Outlet />
       </Main>
--- packages/sanguk/src/components/layout/navbar.tsx
+++ packages/sanguk/src/components/layout/navbar.tsx
@@ -4,7 +4,7 @@ import { useRecoilState } from 'recoil';
 import { Button, Header, Span } from '@/components/common';
 import { themeState } from '@/store';
 
-export default function Gnb({ children }: PropsWithChildren): JSX.Element {
+export default function Navbar({ children }: PropsWithChildren): JSX.Element {
   const [theme, setTheme] = useRecoilState(themeState);
 
   useEffect(() => {
@@ -15,9 +15,9 @@ export default function Gnb({ children }: PropsWithChildren): JSX.Element {
   const toggleTheme = () => setTheme(theme === 'dark' ? 'light' : 'dark');
 
   return (
-    <Header display="flex">
-      <Button display="inline-flex" ml="auto" px="2" py="2" borderT="0" roundedT="full" onClick={toggleTheme}>
-        <Span backgroundColor="main200">theme: {theme}</Span>
+    <Header display={'flex'}>
+      <Button display={'inline-flex'} ml={'auto'} px={'2'} py={'2'} borderT={'0'} roundedT={'full'} onClick={toggleTheme}>
+        <Span backgroundColor={'main200'}>theme: {theme}</Span>
       </Button>
     </Header>
   );
