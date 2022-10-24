947920020c94312abe4f66b5b8f08772086099df
TtaengG <taehyung.kim@myrealtrip.com>
2022-10-24 23:51:51 +0900

    feat: Input Validator 세팅중

--- shared/hooks/useInput/index.tsx
+++ shared/hooks/useInput/index.tsx
@@ -2,7 +2,6 @@ import { useState, useCallback } from 'react';
 
 const useInput = ({ initValue }: UseInputOptions): UseInputReturns => {
   const [value, setValue] = useState<UseInputValue>(initValue || '');
-  const [valid, setValid] = useState(0);
 
   const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     const {
--- shared/hooks/useInput/validators.tsx
+++ shared/hooks/useInput/validators.tsx
@@ -1,9 +1,31 @@
-const Validators: { [k: string]: (v: UseInputValue, ...params: number[]) => boolean } = {
-  required: v => !!v,
-  minLength: (v, l: number) => String(v).length >= l,
-  maxLength: (v, l: number) => String(v).length <= l,
-  email: v => /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(String(v)),
-  KoEn: v => /^[가-힣A-Za-z]$/.test(String(v)),
+type ValidatorKey = 'required' | 'minLength' | 'maxLength' | 'email' | 'KoEn';
+
+interface ValidatorValue {
+  isValid: (v: UseInputValue, ...params: number[]) => boolean;
+  errorMessage: string;
+}
+
+const Validators: { [key in ValidatorKey]?: ValidatorValue } = {
+  required: {
+    isValid: v => !!v,
+    errorMessage: '',
+  },
+  minLength: {
+    isValid: (v, l: number) => String(v).length >= l,
+    errorMessage: '',
+  },
+  maxLength: {
+    isValid: (v, l: number) => String(v).length <= l,
+    errorMessage: '',
+  },
+  email: {
+    isValid: v => /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(String(v)),
+    errorMessage: '',
+  },
+  KoEn: {
+    isValid: v => /^[가-힣A-Za-z]$/.test(String(v)),
+    errorMessage: '',
+  },
 };
 
 export default Validators;
