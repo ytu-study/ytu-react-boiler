855daa49c671fe57100a419e27571457b0e75f12
ukyi <devcat.js@gmail.com>
2022-10-28 23:04:30 +0900

    ✨ NavLink Home, Example Recipes 추가

--- packages/sanguk/src/components/layout/navbar.tsx
+++ packages/sanguk/src/components/layout/navbar.tsx
@@ -1,5 +1,6 @@
 import type { PropsWithChildren } from 'react';
 import { useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { Button, Header, Span } from '@/components/common';
 import { themeState } from '@/store';
@@ -15,10 +16,12 @@ export default function Navbar({ children }: PropsWithChildren): JSX.Element {
   const toggleTheme = () => setTheme(theme === 'dark' ? 'light' : 'dark');
 
   return (
-    <Header display={'flex'}>
+    <Header display={'flex'} flexDirection={'column'}>
       <Button display={'inline-flex'} ml={'auto'} px={'2'} py={'2'} borderT={'0'} roundedT={'full'} onClick={toggleTheme}>
         <Span backgroundColor={'main200'}>theme: {theme}</Span>
       </Button>
+      <NavLink to="/">HOME</NavLink>
+      <NavLink to="/example/recipes">Example Recipes</NavLink>
     </Header>
   );
 }
